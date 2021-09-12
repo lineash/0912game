22a721b19c01ba7654d28101c62da158f60414e6
lineash <olol5454@naver.com>
2021-09-12 21:16:17 +0900

    1

--- 0912game/main.cpp
+++ 0912game/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <bangtal>
+using namespace std;
+using namespace bangtal;
 
 int main() {
+    cout<<"HelloWorld";
+    auto room1 = Scene::create("room1", "room1");
     
+    startGame(room1);
 }
