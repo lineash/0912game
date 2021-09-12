dbe65ef276a1b718bf29e1bee82d65aaf0c243c9
lineash <olol5454@naver.com>
2021-09-12 21:00:51 +0900

    Initial Commit

---
+++ 0912game/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  0912game
+//
+//  Created by line.ash on 2021/09/12.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
